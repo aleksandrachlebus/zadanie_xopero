446d1fc521657b14fcf1aef0900d1f3c76ea3395
Aleksandra <ola.chlebus@gmail.com>
2025-11-26 12:11:45 +0100

    positive and negative logit tests with POM

---
+++ ZadanieXopero/LoginTest.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Xml.Linq;
+using ZadanieXopero.PageObjects;
+
+namespace ZadanieXopero
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+        }
+
+        [Test]
+        public void LoginCorrectCredentialsTest()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            loginPage.LogIn("standard_user", "secret_sauce");
+            Assert.That(driver.Url, Is.EqualTo("https://www.saucedemo.com/inventory.html")); 
+        }
+
+        [Test]
+        public void LoginWrongPasswordTest()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            loginPage.LogIn("standard_user", "wrong_password");
+            Assert.That(loginPage.Error.Text, Is.EqualTo("Epic sadface: Username and password do not match any user in this service"));
+            Assert.That(driver.Url, Is.EqualTo("https://www.saucedemo.com/"));
+        }
+
+        [Test]
+        public void LoginEmptyPasswordTest()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            loginPage.LogIn("standard_user", "");
+            Assert.That(loginPage.Error.Text, Is.EqualTo("Epic sadface: Password is required"));
+            Assert.That(driver.Url, Is.EqualTo("https://www.saucedemo.com/"));
+        }
+
+        [Test]
+        public void LoginEmptyUserTest()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            loginPage.LogIn("", "secret_sauce");
+            Assert.That(loginPage.Error.Text, Is.EqualTo("Epic sadface: Username is required"));
+            Assert.That(driver.Url, Is.EqualTo("https://www.saucedemo.com/"));
+        }
+
+        [Test]
+        public void LoginNotExistingUserTest()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            loginPage.LogIn("not_existing_user", "secret_sauce");
+            Assert.That(loginPage.Error.Text, Is.EqualTo("Epic sadface: Username and password do not match any user in this service"));
+            Assert.That(driver.Url, Is.EqualTo("https://www.saucedemo.com/"));
+        }
+
+        [TearDown]
+        public void TearDown() {
+            driver.Quit(); 
+        }
+    }
+}
---
+++ ZadanieXopero/PageObjects/LoginPage.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZadanieXopero.PageObjects
+{
+    internal class LoginPage
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.Id, Using = "user-name")]
+        private IWebElement userName;
+
+        [FindsBy(How = How.Id, Using = "password")]
+        private IWebElement password;
+
+        [FindsBy(How = How.Id, Using = "login-button")]
+        private IWebElement loginButton;
+
+        [FindsBy(How = How.CssSelector, Using = "[data-test='error']")]
+        private IWebElement error;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public IWebElement Error { get => error; set => error = value; } //seter potrzebny?
+
+        public void LogIn(string user, string pwd)
+        {
+            userName.SendKeys(user);
+            password.SendKeys(pwd);
+            loginButton.Click();
+        }
+    }
+}
